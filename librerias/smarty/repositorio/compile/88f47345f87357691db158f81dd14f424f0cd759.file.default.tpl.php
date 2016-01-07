b005b446377ea6fab7e3e5e4089f9c67acde992a
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2016-01-07 14:06:12 -0600

    Ultimos cambios

--- librerias/smarty/repositorio/compile/88f47345f87357691db158f81dd14f424f0cd759.file.default.tpl.php
+++ librerias/smarty/repositorio/compile/88f47345f87357691db158f81dd14f424f0cd759.file.default.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.11, created on 2015-12-29 09:02:53
+<?php /* Smarty version Smarty-3.1.11, created on 2016-01-07 08:16:58
          compiled from "templates/plantillas/layout/default.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:200058433755e4995bd4c484-25432014%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '88f47345f87357691db158f81dd14f424f0cd759' => 
     array (
       0 => 'templates/plantillas/layout/default.tpl',
-      1 => 1451401367,
+      1 => 1452176142,
       2 => 'file',
     ),
   ),
